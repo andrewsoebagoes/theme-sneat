030dfbe5e88e76480b33dc065fdbd0a2406d1450
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-09-18 16:29:54 +0700

    add footer text

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 				<footer class="content-footer footer bg-footer-theme">
 					<div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
 						<div class="mb-2 mb-md-0">
-							© <?=date('Y')?> made with ❤️ by
+							© <?=date('Y')?> <?= env('FOOTER_TEXT') ?> Theme by
 							<a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
 						</div>
 					</div>
